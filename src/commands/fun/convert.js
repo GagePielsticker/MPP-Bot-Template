7cdc165929ab03737b515c4910020f4a2ccdbf8b
Gage Pielsticker <gagepays@gmail.com>
2022-09-13 20:05:25 -0500

    Update convert.js

--- src/commands/fun/convert.js
+++ src/commands/fun/convert.js
@@ -46,7 +46,7 @@ module.exports = class Convert extends Command {
         client.mpp.sendMessage(`@${msg.author.id} Finished song download. Beginning midi translation AI.`)
         let date = +new Date()
 
-        exec(`env`, {
+        exec(`PATH=$PATH:/root/.nix-profile/bin:/nix/store/sz84dqhk99i6mp1ilj1ja8kyspji0jdl-pianotrans-1.0/bin:/root/.nix-profile/bin:/nix/var/nix/profiles/default/bin && cd ./audio/ && mv \'${data.title}.mp3\' ${date}.mp3 && pianotrans ${date}.mp3`, {
             shell:'/bin/bash'
         }, (err, out) => {
             console.log(err)
